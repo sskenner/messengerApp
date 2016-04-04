951c100a310eb2e8b878069133768a74bda4bba2
santana kenner <sskenner@gmail.com>
2016-04-04 23:22:47 +0000

    create new messages in dB

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # send this request to the Pages controller's home action
   get 'messages' => 'messages#index'
+  get 'messages/new' => 'messages#new'
+  post 'messages' => 'messages#create'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
