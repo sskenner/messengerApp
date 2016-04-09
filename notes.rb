8a3d10cd590bfc0e3909cc51653764694c31bb1e
santana kenner <sskenner@gmail.com>
2016-04-09 19:29:11 +0000

    add notes file

---
+++ notes.rb
@@ -0,0 +1,24 @@
+# codecademy Learn RoR
+
+# Getting Started
+# lesson
+# Get up and running quickly by building a Rails app from scratch.
+# Exercises
+#1.Hello Rails I & II
+$ rails new MySite
+$ bundle install  # installs gems
+$ rails server # rails s -b $IP -p $PORT >> https://community.c9.io/t/running-a-rails-app/1615
+
+# 2.see prev  
+# 3.Controller
+https://www.codecademy.com/articles/request-response-cycle-static
+
+# 4.Routes
+# 5.Views
+# 6.Generalizations
+
+
+# Associations I
+# lesson: Use assocations to model one-to-many relationships.
+
+#Adding more 
